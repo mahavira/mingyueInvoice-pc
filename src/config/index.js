e4a47127c134370e784880a0daf65f0ece455e7e
linmingxiong <272921246@qq.com>
2017-11-02 19:56:04 +0800

    !

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ let env = process.env.NODE_ENV
 let config = {
     env: env,
     title: '明阅电子发票',
-    serverHost: 'http://112.126.75.5',
+    serverHost: '',
     serverRoot: env === 'development' ? '/project_dzff/' : env === 'production' ? '/project_dzff/' : 'https://debug.url.com'
 }
 export default config
